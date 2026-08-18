a4612a02a1eb92bca1260916385c67610c4df9ab
Sayem Chowdhury <sayem314@gmail.com>
2026-08-18 21:54:36 +0600

    Revert "Disable video output for audio-only on iOS"
    This reverts commit 12b580e21531c0deb70630a160d848ed5caf8ea8.

--- packages/vlc-player/ios/VLCPlayerView.swift
+++ packages/vlc-player/ios/VLCPlayerView.swift
@@ -160,11 +160,6 @@ public final class VLCPlayerView: UIView, VLCMediaPlayerDelegate, VLCMediaDelega
             media.addOption(":avcodec-hw=none")
         }
 
-        if audioOnly {
-            // Disable video output for audio-only, mirroring the official app.
-            media.addOption(":no-video")
-        }
-
         media.parse()
         newPlayer.media = media
         player = newPlayer
